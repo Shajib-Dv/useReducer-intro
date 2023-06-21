cba586b30f02d51f4145f695109b53bd047e75fa
Shajib-Dv <mdsojeeb242@gmail.com>
2023-06-21 14:03:16 +0600

    case for handle checkbox

--- src/components/FormCompo/Form.jsx
+++ src/components/FormCompo/Form.jsx
@@ -7,6 +7,9 @@ const Form = () => {
     email: "",
     password: "",
     address: "",
+    quantity: 0,
+    price: 0,
+    term: false,
   };
 
   const reducer = (state, action) => {
@@ -15,6 +18,21 @@ const Form = () => {
         return {
           ...state,
           [action.payload.name]: action.payload.value,
+        };
+      case "INCREMENT":
+        return {
+          ...state,
+          quantity: state.quantity + 1,
+        };
+      case "DECREMENT":
+        return {
+          ...state,
+          quantity: state.quantity - 1,
+        };
+      case "TERM":
+        return {
+          ...state,
+          term: !state.term,
         };
 
       default:
@@ -34,7 +52,7 @@ const Form = () => {
   };
 
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero h-full bg-base-200">
       <div className="hero-content w-1/2 flex-col">
         <div className="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
           <form onSubmit={handleSubmit} className="card-body">
@@ -89,9 +107,65 @@ const Form = () => {
                 }
               />
             </div>
-            <div className="form-control mt-6">
-              <button type="submit" className="btn btn-primary">
-                Login
+            <div className="flex gap-4">
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Quantity</span>
+                </label>
+                <div className="flex gap-2 items-center">
+                  <button
+                    onClick={() => dispatch({ type: "DECREMENT" })}
+                    className="btn btn-primary"
+                  >
+                    -
+                  </button>
+                  <span className="w-full text-center">{state.quantity}</span>
+                  <button
+                    onClick={() => dispatch({ type: "INCREMENT" })}
+                    className="btn btn-primary"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  placeholder="price"
+                  defaultValue={state.price || 0}
+                  className="input input-bordered"
+                  onBlur={(e) =>
+                    dispatch({
+                      type: "INPUT",
+                      payload: {
+                        name: e.target.name,
+                        value: parseInt(e.target.value),
+                      },
+                    })
+                  }
+                />
+              </div>
+            </div>
+            <div className="flex justify-between items-center mt-6">
+              <div className="flex items-center gap-1">
+                <input type="checkbox" name="term" id="term" />
+                <label
+                  onClick={() => dispatch({ type: "TERM" })}
+                  htmlFor="term"
+                >
+                  Accept terms and conditions
+                </label>
+              </div>
+              <button
+                disabled={!state.term}
+                type="submit"
+                className="btn btn-primary"
+              >
+                Submit
               </button>
             </div>
           </form>
